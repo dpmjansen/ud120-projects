d1f190e5aa3e2c6032c026d701a4849bd363695d
David Jansen <dpmjansen@gmail.com>
2017-11-24 11:23:15 +0100

    Migrating to other environment...

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,6 +26,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+model = GaussianNB()
+
+t0 = time()
+model.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+predictions = model.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+print(predictions) 
+accuracy = accuracy_score(predictions,labels_test)
+print(accuracy)
 
 
 #########################################################
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,32 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+
+#reduce data set size
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+#clf = SVC(kernel='linear')
+#clf = SVC(kernel='rbf')
+clf = SVC(kernel='rbf',C=10000.)
+
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+prd = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+print(prd)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(prd,labels_test))
+#print class of (10, 26, 50)
+#print(prd[10])
+#print(prd[26])
+#print(prd[50])
+print((prd == 1).sum())
 
 #########################################################
 
