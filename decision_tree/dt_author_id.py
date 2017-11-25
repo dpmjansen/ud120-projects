33f26ae3a1eba92b0d640691ce65936c25236879
David Jansen <david.jansen@ing.nl>
2017-11-25 01:43:51 +0100

    Added simplified check script, did some homework on decision trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,8 +24,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+print(len(features_train[0]))
+
+clf = DecisionTreeClassifier(min_samples_split=40)
+
+clf.fit(features_train,labels_train)
+
+prd = clf.predict(features_test)
+acc = accuracy_score(prd,labels_test)
 
 
+print(acc)
 #########################################################
 
 
